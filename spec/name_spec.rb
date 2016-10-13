5312c4257f87efd14a02fd06c13225e395cb7ebf
Katy McCann <katy.alice.mccann@gmail.com>
2016-10-13 10:15:34 +0100

    Add web_spec_helper

--- spec/name_spec.rb
+++ spec/name_spec.rb
@@ -3,22 +3,14 @@ require 'spec_helper'
 
 feature 'Start' do
   scenario 'Get players names and display them' do
-    visit "/"
-
-    fill_in :player1, with: 'Li'
-    fill_in :player2, with: 'Katy'
-    click_button "Submit"
-
+    sign_in_and_play
     expect(page).to have_content('Li vs. Katy')
   end
 end
 
 feature 'Hit point information' do
   scenario "allow player1 to see player2's hit points" do
-    visit "/"
-    fill_in :player1, with: 'Li'
-    fill_in :player2, with: 'Katy'
-    click_button "Submit"
+    sign_in_and_play 
     expect(page).to have_content("Katy's hit points: 60/60")
   end
 end
