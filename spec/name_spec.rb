5b09b0f0f470a56400f28254f68e79fd1e515d94
Katy McCann <katy.alice.mccann@gmail.com>
2016-10-13 10:52:47 +0100

    Create attack button

--- spec/name_spec.rb
+++ spec/name_spec.rb
@@ -10,7 +10,15 @@ end
 
 feature 'Hit point information' do
   scenario "allow player1 to see player2's hit points" do
-    sign_in_and_play 
+    sign_in_and_play
     expect(page).to have_content("Katy's hit points: 60/60")
   end
+end
+
+  feature 'There is an attack button' do
+    scenario "it allows player1 to attack player2" do
+      sign_in_and_play
+      click_button('Attack')
+      expect(page).to have_content('Attack Successfull!')
+    end
 end
